9333736159592d3a9d7ad6a025433865cd265bd2
santiago <santiagotovar.contacto@gmail.com>
2023-07-27 11:12:02 -0500

    feat: Tasks render added.

--- client/src/components/TasksList.jsx
+++ client/src/components/TasksList.jsx
@@ -1,12 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import {getAllTasks} from '../api/tasks.api'
+import TaskCard from "./TaskCard"
 
 export default function TasksList() {
 
+    const [tasks, setTasks] = useState([])
+
     useEffect(() => {
-        
+        async function loadTasks(){
+          const res = await getAllTasks()
+          setTasks(res.data)
+        }
+        loadTasks()
     }, [])
 
   return (
-    <div>TasksList</div>
+    <div>{tasks.map((task => {
+        return <TaskCard task={task} key={task.id} />
+
+    }))}</div>
   )
 }
